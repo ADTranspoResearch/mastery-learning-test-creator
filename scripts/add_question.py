cf692b97a7eda75f1c0d8f204f613a2c649b9a6e
ADTranspoResearch <alessandro.dapice@mail.mcgill.ca>
2026-04-16 18:13:22 -0400

    basic question saving functionality

---
+++ scripts/add_question.py
@@ -0,0 +1,10 @@
+import _setup_path #pylint: disable=unused-import
+
+from mastery.questions.open_question_db import save_question_db, open_question_db
+from mastery.questions.save_question import add_question
+
+question_df = open_question_db()
+
+question_df = add_question(question_df)
+
+save_question_db(question_df)
